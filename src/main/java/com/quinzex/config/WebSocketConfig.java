4e18096d99a3347df0b7d0cdb9f27e23f3282009
akhil <malisettybhargavakhil25@gmail.com>
2026-03-26 12:14:00 +0530

    updated websocket sec v1

--- src/main/java/com/quinzex/config/WebSocketConfig.java
+++ src/main/java/com/quinzex/config/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry endpointRegistry){
         endpointRegistry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("https://www.careervedha.com").withSockJS();
+                .setAllowedOriginPatterns("*").withSockJS();
 
 
     }
